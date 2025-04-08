252575c3f015cfccd57f9571a8acc84db21c9dd6
jeferson <jeferson.oferreira@etec.sp.gov.br>
2025-04-08 20:23:43 -0300

    aula 0804

---
+++ CA0804/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA0804
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tamanho = 5;
+            int[] lista = new int[tamanho];
+            int i;
+            for (i = 0; i < tamanho; i++)
+            {
+                Console.WriteLine("Digite um valor:");
+                lista[i] = int.Parse(Console.ReadLine());
+            }
+            for (i = 0; i < tamanho; i++)
+            {
+            Console.WriteLine("Valor:{0}", lista[i]);
+            }
+        }
+    }
+}
